5d1ca97a09c65411a66569b5315da04801de8eb4
Hwangwonuk <hok0173@naver.com>
2023-05-24 23:09:16 +0900

    variables, const and let

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+// const = 상수
+// let = 변수
+// var = 지양해야할 옛변수 let과 같음
+const a = 5;
+const b = 2;
+let myName = "uk";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a % b);
+console.log("hello " + myName);
+
+myName = "wonuk";
+
+console.log("your new name is " + myName);
