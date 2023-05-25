0f9d7432d2860d9cde321b0715dc154cc185d8d3
Hwangwonuk <hok0173@naver.com>
2023-05-25 23:08:50 +0900

    conditionals, recap

--- momentum/app.js
+++ momentum/app.js
@@ -1,30 +1,19 @@
-// return
-const age = 96;
+// parseInt(age);  string to convert into a number;
 
-function calculateKrAge(ageOfForeigner) {
-  return ageOfForeigner + 2;
+const age = parseInt(prompt("How old are you>"));
+
+if (isNaN(age) || age < 0) {
+  console.log("Please write a number");
+} else if (age < 18) {
+  console.log("You are too young");
+} else if (age >= 18 && age <= 50) {
+  console.log("You can drink")
+} else if (age > 50 && age <= 80) {
+  console.log("You should exercise");
+} else if (age === 100) { // 적는 순서를 주의하자.
+  console.log("wow you are wise");
+} else if (age > 80) {
+  console.log("You can do whatever you want.");
 }
 
-const krAge = calculateKrAge(age);
 
-console.log(krAge); // 98
-
-const calculator = {
-  plus: function(a, b) {
-    return console.log(a + b);
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  times: function (a, b) {
-    return a * b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  power: function(a, b) {
-    return a ** b
-  },
-};
-
-const plusResult = calculator.plus(2, 3);
