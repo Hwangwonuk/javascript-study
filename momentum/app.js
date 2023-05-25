73a1f3f92d76b47bf7b6763a8c17680e109f8eef
Hwangwonuk <hok0173@naver.com>
2023-05-26 00:00:56 +0900

    document object, html in javascript, searching for elements

--- momentum/app.js
+++ momentum/app.js
@@ -1,19 +1,9 @@
-// parseInt(age);  string to convert into a number;
+const title = document.getElementById("something");
 
-const age = parseInt(prompt("How old are you>"));
+const sameTitle = document.querySelector(".hello");
 
-if (isNaN(age) || age < 0) {
-  console.log("Please write a number");
-} else if (age < 18) {
-  console.log("You are too young");
-} else if (age >= 18 && age <= 50) {
-  console.log("You can drink")
-} else if (age > 50 && age <= 80) {
-  console.log("You should exercise");
-} else if (age === 100) { // 적는 순서를 주의하자.
-  console.log("wow you are wise");
-} else if (age > 80) {
-  console.log("You can do whatever you want.");
-}
+title.innerText = "Got you!";
 
+console.log(title.id);
+console.log(title.className);
 
