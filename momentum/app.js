e37507e212a41c42a071c04a8421906ca63c04e3
Hwangwonuk <hok0173@naver.com>
2023-05-26 01:04:01 +0900

    Events

--- momentum/app.js
+++ momentum/app.js
@@ -1,9 +1,42 @@
-const title = document.getElementById("something");
+const h1 = document.querySelector(".hello:first-child h1");
 
-const sameTitle = document.querySelector(".hello");
+function handleTitleClick() {
+  console.log("h1 was clicked!");
+  h1.style.color = "blue";
+}
 
-title.innerText = "Got you!";
+function handleMouseEnter() {
+  h1.innerText = "mouse is here!";
+}
 
-console.log(title.id);
-console.log(title.className);
+function handleMouseLeave() {
+  h1.innerText = "Mouse is gone!";
+}
 
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("copier!")
+}
+
+function handleWindowOffline() {
+  alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+  alert("All GOOD");
+}
+
+// addEventListener를 사용하면 removeEventListener로 삭제할 수 있다.
+
+// h1.addEventListener("click", handleTitleClick);
+h1.onclick = handleTitleClick;
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
