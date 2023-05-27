2d3df1675410520d6b8bb3412b5c3f1338bc688d
Hwangwonuk <hok0173@naver.com>
2023-05-28 00:10:28 +0900

    css in javascript part two, three

--- momentum/app.js
+++ momentum/app.js
@@ -1,14 +1,13 @@
 const h1 = document.querySelector(".hello:first-child h1");
 
 function handleTitleClick() {
-  const currentColor = h1.style.color;
-  let newColor;
-  if (currentColor === "blue") {
-    newColor = "tomato";
-  } else {
-    newColor = "blue";
-  }
-  h1.style.color = newColor;
+  h1.classList.toggle("clicked");
+  // const clickedClass = "clicked";
+  // if (h1.classList.contains(clickedClass)) {
+  //   h1.classList.remove(clickedClass);
+  // } else {
+  //   h1.classList.add(clickedClass);
+  // }
 }
 
 // addEventListener를 사용하면 removeEventListener로 삭제할 수 있다.
