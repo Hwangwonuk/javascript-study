334b4f9edfbf5e3da4d02eae419def24c2365223
Hwangwonuk <hok0173@naver.com>
2023-06-01 00:46:23 +0900

    Events chapter2 수정

--- momentum/app.js
+++ momentum/app.js
@@ -1,9 +1,9 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
-function onLoginSubmit(tomato) {
-  tomato.preventDefault(); // 브라우저가 기본 동작을 실행하지 못하게 막기 event object는 preventDefault함수를 기본적으로 갖고 있음
-  console.log(tomato);
+function onLoginSubmit(event) {
+  event.preventDefault(); // 브라우저가 기본 동작을 실행하지 못하게 막기 event object는 preventDefault함수를 기본적으로 갖고 있음
+  console.log(event);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit); // submit 이벤트가 발생한다면, onLoginSubmit함수를 실행시킨다는 의미 JS는 onLoginSubmit함수 호출시 인자를 담아서 호출함. 해당 인자는 event object를 담은 정보들
