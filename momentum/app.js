0a15457617fc8df2a4f7de3265f5f5c0aab3a1c4
Hwangwonuk <hok0173@naver.com>
2023-05-31 23:58:55 +0900

    Events chapter1

--- momentum/app.js
+++ momentum/app.js
@@ -1,13 +1,13 @@
-const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
-
-function handleLoginBtnClick() {
-  const username = loginInput.value;
-  // if (username === "") {
-  //   alert("Please write tour name");
-  // } else if(username.length > 15) {
-  //   alert("Your name is too long.");
-  // }
-  console.log(username);
-}
-loginButton.addEventListener("click", handleLoginBtnClick)
+// const loginInput = document.querySelector("#login-form input");
+// const loginButton = document.querySelector("#login-form button");
+//
+// function handleLoginBtnClick() {
+//   const username = loginInput.value;
+//   // if (username === "") {
+//   //   alert("Please write tour name");
+//   // } else if(username.length > 15) {
+//   //   alert("Your name is too long.");
+//   // }
+//   console.log(username);
+// }
+// loginButton.addEventListener("click", handleLoginBtnClick)
