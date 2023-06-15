6e0aae5cd6466a77801f8b8e9369bd79c4655c73
Hwangwonuk <hok0173@naver.com>
2023-06-16 00:03:50 +0900

    Timeouts and Dates

--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -1,7 +1,9 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-  console.log("hello");
+function getClock() {
+  const date = new Date();
+  clock.innerText =`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-setTimeout(sayHello, 5000);
+getClock(); // 즉시호출
+setInterval(getClock, 1000); // load된 후 1초마다 호출
