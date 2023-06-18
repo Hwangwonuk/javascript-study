3241286e18da74483bd01eee9ed4573b5c762d87
Hwangwonuk <wonuk_hwang@bigin.io>
2023-06-18 13:06:14 +0900

    Revert "padStart()"
    This reverts commit acd3fea4d47745a5b17e1b7de4e6dab5f44a6ede.

--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -2,11 +2,7 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
   const date = new Date();
-  // padStart(2, "0") : string이 가져야 하는 길이를 2로 설정하고 그렇지 않다면 string의 앞쪽에 0을 추가하도록 한다.
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText =`${hours}:${minutes}:${seconds}`;
+  clock.innerText =`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
 getClock(); // 즉시호출
