ed291b687631aef42c9eb294558f6f650f2e3573
Hwangwonuk <hok0173@naver.com>
2023-06-21 00:59:23 +0900

    Adding ToDos

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -2,12 +2,19 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
+function paintToDo(newToDo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newToDo;
+  toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newToDo = toDoInput.value;
-  console.log(toDoInput.value);
   toDoInput.value = "";
-  console.log(newToDo, toDoInput.value); // toDoInput.value 는 빈값이다.
+  paintToDo(newToDo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
