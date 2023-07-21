a2de4dcc82b29cb24a95668069ad790a84d1f902
Hwangwonuk <hok0173@naver.com>
2023-07-21 19:05:11 +0900

    Saving Image

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -1,3 +1,4 @@
+const saveBtn = document.getElementById("save");
 const textInput = document.getElementById("text");
 const fileInput = document.getElementById("file");
 const modeBtn = document.getElementById('mode-btn');
@@ -103,6 +104,14 @@ function onDoubleClick(event) {
   }
 }
 
+function onSaveClick() {
+  const url = canvas.toDataURL();
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "myDrawing.png";
+  a.click();
+}
+
 canvas.addEventListener("dblclick", onDoubleClick);
 // canvas.onmousemove = onMove; 아래와 같다.
 canvas.addEventListener("mousemove", onMove);
@@ -120,3 +129,4 @@ modeBtn.addEventListener("click", onModeClick);
 destroyBtn.addEventListener("click", onDestroyClick);
 eraserBtn.addEventListener("click", onEraserClick);
 fileInput.addEventListener("change", onFileChange);
+saveBtn.addEventListener("click", onSaveClick);
